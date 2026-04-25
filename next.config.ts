72d17f67339b88ce45d566c90599ce8d17ec7610
Abhij134 <148138113+Abhij134@users.noreply.github.com>
2026-04-26 01:30:26 +0530

    chore: remove eslint ignoreDuringBuilds

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
+
 export default nextConfig;
